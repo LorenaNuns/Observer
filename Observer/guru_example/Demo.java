b628217aa040f224d7dfcb896dc5d213e04551a2
Lorena Nunes <lorena.nunes12@hotmail.com>
2022-06-02 16:11:42 -0300

    oi
    commit

---
+++ Observer/guru_example/Demo.java
@@ -0,0 +1,20 @@
+package guru_example;
+
+import guru_example.editor.Editor;
+import guru_example.listeners.EmailNotificationListener;
+import guru_example.listeners.LogOpenListener;
+
+public class Demo {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        editor.events.subscribe("open", new LogOpenListener("/path/to/log/file.txt"));
+        editor.events.subscribe("save", new EmailNotificationListener("admin@example.com"));
+
+        try {
+            editor.openFile("test.txt");
+            editor.saveFile();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
